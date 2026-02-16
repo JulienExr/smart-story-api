ff933aed0bfac5cfe12fdd6c38c0bdd3a6a58c9f
Julien <julien.excoffier@ecole.ensicaen.fr>
2026-02-16 06:03:53 +0100

    Remove setup.sh script as it is no longer needed for environment configuration

--- setup.sh
+++
@@ -1,7 +0,0 @@
-echo "⚙️  Configuration de l'environnement de build..."
-pip install "setuptools<70.0.0" wheel
-
-echo "🚀 Installation des dépendances du projet..."
-pip install -r requirements.txt
-
-echo "✅ Installation terminée avec succès !"
